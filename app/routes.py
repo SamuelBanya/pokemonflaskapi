e3d3500880378cd2953bfe648329bc1d5999352e
Samuel Banya <sbanya@fastmail.com>
2025-06-11 01:59:26 -0500

    Adding color changes with badges and other aesthetic changes

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,27 @@ from app.models import Pokemon
 
 bp = Blueprint('main', __name__)
 
+type_colors = {
+    "normal": "#A8A77A",
+    "fire": "#EE8130",
+    "water": "#6390F0",
+    "electric": "#F7D02C",
+    "grass": "#7AC74C",
+    "ice": "#96D9D6",
+    "fighting": "#C22E28",
+    "poison": "#A33EA1",
+    "ground": "#E2BF65",
+    "flying": "#A98FF3",
+    "psychic": "#F95587",
+    "bug": "#A6B91A",
+    "rock": "#B6A136",
+    "ghost": "#735797",
+    "dragon": "#6F35FC",
+    "dark": "#705746",
+    "steel": "#B7B7CE",
+    "fairy": "#D685AD"
+}
+
 @bp.route("/")
 def index():
     return render_template('index.html')
@@ -14,4 +35,4 @@ def show_pokemon(pokemonname):
     if not results:
         return render_template('pokemonnotfound.html', pokemonname=pokemonname)
 
-    return render_template('pokemonflaskapi.html', pokemonname=pokemonname, pokemon=results)
+    return render_template('pokemonflaskapi.html', pokemonname=pokemonname, pokemon=results, type_colors=type_colors)
